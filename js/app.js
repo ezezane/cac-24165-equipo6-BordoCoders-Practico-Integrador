bc220c80d9ad3f26819104d9730714b29e5a1de6
GMaidana95 <gal.maidana95@gmail.com>
2024-05-11 01:05:23 -0300

    11/05 Unif css & product.html add

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let contenedorProductos = $("#contenedor-productos");
+
+mostrarproductos(productos);
+
+function mostrarproductos(productos) {
+  contenedorProductos.html("");
+
+  for (const producto of productos) {
+    contenedorProductos.append(`
+            <div class="card-productos">
+                <div>
+                    <img class="producto-img" src=${producto.img} alt="producto-${producto.nombre}">
+                    <p class="producto-marca">${producto.marca}</p>
+                    <p class="producto-nombre">${producto.nombre}</p>
+                    <p class="producto-precio">$ ${producto.precio}</p>
+                </div>
+            </div>
+            `);
+  }
+}
